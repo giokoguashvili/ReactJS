868658fd4a33a17c1c4c80ea273ca7fd640a09ed
kogoia <kogo27@gmail.com>
2016-11-25 16:57:34 +0430

    add babel presets

---
+++ 1-react-template/src/js/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render(){
+        return (
+            <div>
+                Welcome!
+            </div>
+        );
+    }
+}
+
+export default Layout
--- 1-react-template/webpack.config.js
+++ 1-react-template/webpack.config.js
@@ -1,5 +1,17 @@
 let config = {
-    entry : './src/js/app.jsx',
+    entry : './src/js/app.js',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/, 
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }   
+            }
+        ]
+    },
     output: { 
         path: './src',
         filename: 'app.bundle.js'
