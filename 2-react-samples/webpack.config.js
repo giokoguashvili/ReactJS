cf1549ccc69fa2bd96d31e79caffdcbb8fa8dea9
kogoia <kogo27@gmail.com>
2016-11-28 19:06:44 +0400

    add folder for react-samples

---
+++ 2-react-samples/webpack.config.js
@@ -0,0 +1,35 @@
+let path = require("path");
+
+let config = {
+    context: path.join(__dirname, "./"),
+    entry : {
+        main: './src/js/app.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/, 
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }   
+            }
+        ]
+    },
+    output: { 
+        path: './src',
+        filename: 'app.bundle.js'
+    },
+    devServer:
+    {
+        inline: true,
+        port: 2727,
+    },
+    // devtool: "eval"
+};
+
+module.exports = config;
