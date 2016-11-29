daafc8efa75f34eed71678ce6ed408a13e844c7b
kogoia <kogo27@gmail.com>
2016-11-29 21:20:09 +0400

    change Header componetn to lamda style

--- 2-react-samples/src/js/components/Header.jsx
+++ 2-react-samples/src/js/components/Header.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
-export default class Header extends Component {
-    render(){
-        return (
-            <header>
-                <h1>Welcome!</h1>
-            </header>
-        );
-    }
-};
+// export default class Header extends Component {
+//     render(){
+//         return (
+//             <header>
+//                 <h1>Welcome!</h1>
+//             </header>
+//         );
+//     }
+// };
+
+
+export default props =>(
+                    <header>
+                        <h1>Welcome!</h1>
+                    </header>
+                );
--- 2-react-samples/src/js/components/Layout.jsx
+++ 2-react-samples/src/js/components/Layout.jsx
@@ -3,10 +3,25 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends Component {
+    constructor(){
+        super();
+        this.count = 0;
+        this.state = { name : "gio", count : 1 };
+    }
     render(){
+        this.count += 1;
+        setTimeout(() => {
+             this.count += 2;
+           // this.setState({ name : "gio", count : 1 });
+            console.log(this.count);
+        }, 1000);
         return (
             <div>
                 <Header/>
+                <div>
+                    {this.state.name} 
+                </div>
+                {this.count} 
                 <Footer/>
             </div>
         );
