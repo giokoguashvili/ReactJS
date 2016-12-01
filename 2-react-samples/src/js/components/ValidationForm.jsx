cc6d97e1b8acf828c1e7d7939c627712be28f800
kogoia <kogo27@gmail.com>
2016-12-01 11:33:17 +0400

    use refs in validationForm component

--- 2-react-samples/src/js/components/ValidationForm.jsx
+++ 2-react-samples/src/js/components/ValidationForm.jsx
@@ -3,6 +3,19 @@ import React, { Component } from 'react';
 class ValidationForm extends Component {
     constructor(props) {
         super(props);
+        this.update = this.update.bind(this);
+        this.state = {
+            input1 : '',
+            input2 : ''
+        }
+    }
+
+    update(e) {
+        console.log(this.refs);
+        this.setState({
+            input1 : this.refs.a.value,
+            input2 : this.refs.b.value
+        })
     }
 
     render() {
@@ -12,6 +25,10 @@ class ValidationForm extends Component {
                     {this.props.labelName}:
                     <input type="text"/>
                 </label>
+                <br/>
+                <input ref="a" id="1" type="text" onChange={this.update} /> {this.state.input1}
+                <br/>
+                <input ref="b" id="2" type="text" onChange={this.update} /> {this.state.input2}
             </form>
         );
     }
