c602b513e28939da9b1f20d3bcfbcca8533678a8
kogoia <kogo27@gmail.com>
2016-12-05 15:18:08 +0400

    change project structure and webpackConfiges

--- 4-react-flux/webpack.config.js
+++ 4-react-flux/webpack.config.js
@@ -1,40 +1,46 @@
 let path = require("path");
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 let config = {
+
     context: path.join(__dirname, "./"),
+
     entry : {
-        main: './src/js/app.js'
+        main: './src/main.js'
     },
+
     resolve: {
         extensions: ['', '.js', '.jsx'],
     },
+
     module: {
         loaders: [
             {
-                test: /\.jsx?$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/, 
                 loader: "babel-loader",
-                passPerPreset: true,
                 query: {
-                    presets: ['es2015', 'react'],
-                    plugins: [
-                         ["typecheck", {
-                                "disable": {
-                                    "production": true
-                                }
-                            }
-                        ]
-                    ]
+                    presets: ['es2015', 'react']
                 }   
             }
         ]
     },
-    plugins: [new OpenBrowserPlugin({ url: 'http://localhost:2727' })],
+
+    plugins: [
+        new OpenBrowserPlugin({ url: 'http://localhost:2727' }),
+        new HtmlWebpackPlugin({
+            title: 'React with Flux',
+            template: './src/index.ejs', 
+        })
+    ],
+
     output: { 
-        path: './src',
+        path: './build',
         filename: 'app.bundle.js'
     },
+
     devServer:
     {
         inline: true,
