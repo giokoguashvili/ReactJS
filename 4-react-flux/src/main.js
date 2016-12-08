0e1c07efb317c91324354b2fe5aff7ab077a865a
kogoia <kogo27@gmail.com>
2016-12-08 11:45:35 +0400

    add drawer menu

--- 4-react-flux/src/main.js
+++ 4-react-flux/src/main.js
@@ -8,17 +8,27 @@ import About from './About';
 import Repos from './Repos';
 import Other from './Other';
 import Home from './Home';
+import Layout from './Layout';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// ReactDOM.render(
+//     <Router history={browserHistory}>
+//         <Route path="/" component={App}>
+//             <IndexRoute component={Home} />
+//             <Route path="/About" component={About}/>
+//             <Route path="/Repos" component={Repos}>
+//                 <Route path="/Repos/:userName/:repoName" component={Repos}/>
+//             </Route>
+//             <Route path="/Other/:repoName/:userName" component={Other}/>
+//         </Route>
+//     </Router>,
+//     document.getElementById("app")
+// );
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Home} />
-            <Route path="/About" component={About}/>
-            <Route path="/Repos" component={Repos}>
-                <Route path="/Repos/:userName/:repoName" component={Repos}/>
-            </Route>
-            <Route path="/Other/:repoName/:userName" component={Other}/>
-        </Route>
-    </Router>,
+    <Layout/>,
     document.getElementById("app")
 );
