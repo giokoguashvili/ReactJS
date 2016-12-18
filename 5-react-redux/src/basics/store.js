d00f38eff16050b0d8fc91b74707680cecc1c7c5
kogoia <kogo27@gmail.com>
2016-12-18 14:18:20 +0400

    add store tests

--- 5-react-redux/src/basics/store.js
+++ 5-react-redux/src/basics/store.js
@@ -6,11 +6,14 @@
 
      update(action){ 
          this._state = this._reducer(this._state, action);
+         return this;
      }
 
      getState() {
          return this._state;
      }
  }
+
+ module.exports = Store;
 
  
