c3392db7bf35ad3c6809daec282174245fb54d3c
kogoia <kogo27@gmail.com>
2016-12-18 13:42:07 +0400

    add actions in reducer

--- 5-react-redux/src/basics/reducer.js
+++ 5-react-redux/src/basics/reducer.js
@@ -3,18 +3,18 @@ var assert = require('chai').assert;
 let stateBefore = 0;
 
 const updateState = (state = 0, action) => {
-    switch (action) {
+    switch (action.type) {
         case 'INC':
-            return state + 1;
+            return state + action.count;
         case 'DEC':
-            return state - 1;
+            return state - action.count;
         default:
             return state;
     }
 };
 
-assert.equal(updateState(0,'INC'), 1);
-assert.equal(updateState(1,'INC'), 2);
-assert.equal(updateState(1,'DEC'), 0);
+assert.equal(updateState(0, { type: 'INC', count: 2 }), 2);
+assert.equal(updateState(1, { type: 'SOME_ACTION' }), 1);
+assert.equal(updateState(1, { type: 'DEC', count: 2 }), -1);
 
 console.log('Tests passed!');
