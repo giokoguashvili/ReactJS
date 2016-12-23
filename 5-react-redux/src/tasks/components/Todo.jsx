724816d23a1f7bb59a150ebeacaab5984e837d2d
kogoia <kogo27@gmail.com>
2016-12-23 10:30:54 +0400

    add checkbox on todo

--- 5-react-redux/src/tasks/components/Todo.jsx
+++ 5-react-redux/src/tasks/components/Todo.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react'
 import { ListItem } from 'material-ui/List'
 import FlatBotton from 'material-ui/FlatButton'
 import FontIcon from 'material-ui/FontIcon';
+import Checkbox from 'material-ui/Checkbox'
+import { blue500, red500, greenA200 } from 'material-ui/styles/colors';
 
 class Todo extends Component {
     constructor(props) {
@@ -11,11 +13,23 @@ class Todo extends Component {
     render() {
         return (
             <ListItem
-                onClick={() => this.props.onTodoClick(this.props.id)}
+                leftCheckbox={
+                    <Checkbox
+                        checked={this.props.completed}
+                        onCheck={() => this.props.onTodoClick(this.props.id)}
+                    />
+                }
                 rightIconButton={
                     <FlatBotton
                         onClick={() => this.props.onDeleteBtnClick(this.props.id)}
-                        icon={<FontIcon className="material-icons">delete</FontIcon>}
+                        icon={
+                            <FontIcon 
+                                className="material-icons"
+                                color={red500}
+                            >
+                                delete
+                            </FontIcon>
+                        }
                         style={{marginTop: 5}}
                     />
                 }
