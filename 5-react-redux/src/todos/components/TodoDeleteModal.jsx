3f01e1e8e654d760bb70d37128a429635fef4200
kogoia <kogo27@gmail.com>
2017-01-12 22:20:19 +0400

    add deleteTodoModal

---
+++ 5-react-redux/src/todos/components/TodoDeleteModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dialog from 'material-ui/Dialog'
+
+const TodoDeleteModal = ({ }) => {
+        return (
+            <Dialog
+                title="delete todo"
+                open={true}
+            />
+        );
+}
+
+export default TodoDeleteModal;
