29ffbefdfb0e7677b25ed86d14dc324d0f6f1430
Giorgi Koguashvili <kogo27@gmail.com>
2017-02-03 13:45:15 +0400

    saves user in db

---
+++ 6-react-redux-express/knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'reddice',
+      user: 'postgres',
+      password: '1'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- 6-react-redux-express/server/routes/users.js
+++ 6-react-redux-express/server/routes/users.js
@@ -2,16 +2,29 @@ import express from 'express'
 import isEmpty from 'lodash/isEmpty'
 
 import validateInput from '../shared/validations/signup'
+import User from '../models/user'
 
 let router = express.Router();
 
 router.post('/', (req, res) => {
     const { errors, isValid } = validateInput(req.body);
 
-    if (!isValid) {
-        res.status(400).json(errors);
+    if (isValid) {
+        //res.json({ success: true });
+        const { username, password: password_digest, email } = req.body;
+        User.forge({
+            username,
+            password_digest,
+            email
+        },
+        {
+            hasTimestamps: true
+        })
+        .save()
+        .then(user => { res.json({ success: true }); })
+        .catch(err => res.status(500).json({ error: err }));
     } else {
-        res.json({ success: true });
+        res.status(400).json(errors);
     }
 });
 
